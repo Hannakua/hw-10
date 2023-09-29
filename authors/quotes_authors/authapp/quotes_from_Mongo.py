76b042735cf80209a7d1e2951ef3cbea7783d4be
Hannakua <126361524+Hannakua@users.noreply.github.com>
2023-09-30 00:42:12 +0300

    mongo

--- authors/quotes_authors/authapp/quotes_from_Mongo.py
+++ authors/quotes_authors/authapp/quotes_from_Mongo.py
@@ -1,23 +1,9 @@
-# from mongoengine import connect
 
-# try:
-#     connect(
-#         host="mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"
-#     )
-#     print("ok connection")
-# except:
-#     print("no connection")
+# client = MongoClient(
+#     "mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"
+# )
 
 
-from pymongo import MongoClient
+# mongoexport --uri="mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"  --collection=authors  --out=authors.json 
 
-
-client = MongoClient(
-    "mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"
-)
-
-db=client.database_name
-c=db.collection_name // This is used to check whether the collection is there or not.
-
-
-mongoexport --uri="mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"  --collection=authors  --out=authors.json 
+# mongoexport --uri="mongodb+srv://annka:PPBVqTIHCRrtHgMy@cluster0.7dlllzi.mongodb.net/?retryWrites=true&w=majority"  --collection=qoutes  --out=quotes.json
